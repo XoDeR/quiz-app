a9f69f113d293aba3974ac5b666cf435010478d7
Volodymyr <syvochka64@gmail.com>
2021-01-11 20:44:42 +0100

    Updates

---
+++ precache-manifest.1771e78de7a508611b4afeea9a999bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b023dae0734e1d75d211ae8302bc043",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "073a420cafc1ba8133d3",
+    "url": "/quiz-app/static/css/main.270bfac1.chunk.css"
+  },
+  {
+    "revision": "a0ab9267520533627b22",
+    "url": "/quiz-app/static/js/2.7e8e639f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.7e8e639f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073a420cafc1ba8133d3",
+    "url": "/quiz-app/static/js/main.815525a4.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
